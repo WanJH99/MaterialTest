22400acac5783ebd3bf295bf8b790e006f46b224
WanJH <337040669@qq.com>
2017-05-07 18:48:46 +0800

    First commit.

--- app/src/main/java/com/example/materialtest/Register.java
+++ app/src/main/java/com/example/materialtest/Register.java
@@ -1,70 +1,99 @@
 package com.example.materialtest;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.litepal.LitePal;
-
 public class Register extends AppCompatActivity {
-
-    private Button register_sure;
-
-    private Button register_cancel;
-
+    private EditText mAccount;                        //用户名编辑
+    private EditText mPwd;                            //密码编辑
+    private EditText mPwdCheck;                       //密码编辑
+    private Button mSureButton;                       //确定按钮
+    private Button mCancelButton;                     //取消按钮
+    private UserDataManager mUserDataManager;         //用户数据管理类
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        //隐藏自带标题栏
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
-            actionBar.hide();
+        mAccount = (EditText) findViewById(R.id.resetpwd_edit_name);
+        mPwd = (EditText) findViewById(R.id.resetpwd_edit_pwd_old);
+        mPwdCheck = (EditText) findViewById(R.id.resetpwd_edit_pwd_new);
+
+        mSureButton = (Button) findViewById(R.id.register_btn_sure);
+        mCancelButton = (Button) findViewById(R.id.register_btn_cancel);
+
+        mSureButton.setOnClickListener(m_register_Listener);      //注册界面两个按钮的监听事件
+        mCancelButton.setOnClickListener(m_register_Listener);
+
+        if (mUserDataManager == null) {
+            mUserDataManager = new UserDataManager(this);
+            mUserDataManager.openDataBase();                              //建立本地数据库
         }
-        register_sure = (Button) findViewById(R.id.register_btn_sure);
-        register_sure.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Intent intent = new Intent(Register.this, HelloWorldActivity.class);
-                //startActivity(intent);
-                LitePal.getDatabase();
-                EditText register_user_name = (EditText) findViewById(R.id.user_name);
-                final String UserName = register_user_name.getText().toString();
-
-                EditText register_user_password1 = (EditText) findViewById(R.id.user_password_first);
-                final String UserPassword1 = register_user_password1.getText().toString();
-
-                EditText register_user_password2 = (EditText) findViewById(R.id.user_password_second);
-                final String UserPassword2 = register_user_password2.getText().toString();
-
-                if (UserPassword1.equals(UserPassword2)==true){
-                    User_Information user = new User_Information();
-                    user.setUser_name(UserName);
-                    user.setUser_password(UserPassword1);
-                    user.save();
-                    Toast.makeText(getApplicationContext(), "注册成功", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(Register.this, Login.class);
-                    startActivity(intent);
-                }else {
-                    Toast.makeText(getApplicationContext(), "两次密码输入不一致", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        register_cancel = (Button) findViewById(R.id.register_btn_cancel);
-        register_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Register.this, Login.class);
-                startActivity(intent);
-            }
-        });
-
 
     }
+    View.OnClickListener m_register_Listener = new View.OnClickListener() {    //不同按钮按下的监听事件选择
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.register_btn_sure:                       //确认按钮的监听事件
+                    register_check();
+                    break;
+                case R.id.register_btn_cancel:                     //取消按钮的监听事件,由注册界面返回登录界面
+                    Intent intent_Register_to_Login = new Intent(Register.this,Login.class) ;    //切换User Activity至Login Activity
+                    startActivity(intent_Register_to_Login);
+                    finish();
+                    break;
+            }
+        }
+    };
+    public void register_check() {                                //确认按钮的监听事件
+        if (isUserNameAndPwdValid()) {
+            String userName = mAccount.getText().toString().trim();
+            String userPwd = mPwd.getText().toString().trim();
+            String userPwdCheck = mPwdCheck.getText().toString().trim();
+            //检查用户是否存在
+            int count=mUserDataManager.findUserByName(userName);
+            //用户已经存在时返回，给出提示文字
+            if(count>0){
+                Toast.makeText(this, getString(R.string.name_already_exist, userName),Toast.LENGTH_SHORT).show();
+                return ;
+            }
+            if(userPwd.equals(userPwdCheck)==false){     //两次密码输入不一样
+                Toast.makeText(this, getString(R.string.pwd_not_the_same),Toast.LENGTH_SHORT).show();
+                return ;
+            } else {
+                UserData mUser = new UserData(userName, userPwd);
+                mUserDataManager.openDataBase();
+                long flag = mUserDataManager.insertUserData(mUser); //新建用户信息
+                if (flag == -1) {
+                    Toast.makeText(this, getString(R.string.register_fail),Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, getString(R.string.register_success),Toast.LENGTH_SHORT).show();
+                    Intent intent_Register_to_Login = new Intent(Register.this,Login.class) ;    //切换User Activity至Login Activity
+                    startActivity(intent_Register_to_Login);
+                    finish();
+                }
+            }
+        }
+    }
+    public boolean isUserNameAndPwdValid() {
+        if (mAccount.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.account_empty),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (mPwd.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.pwd_empty),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(mPwdCheck.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.pwd_check_empty),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
 }
+
